aa2d2463187c9de57ee03f6d67d54f9d0e3efa74
SebastianSumi <cspam7780@gmail.com>
2025-08-22 16:27:55 -0500

    Cambio real 1

---
+++ Asistencia/src/main/java/pe/edu/upeu/asistencia/Aplication.java
@@ -0,0 +1,8 @@
+package pe.edu.upeu.asistencia;
+
+public class Aplication {
+    public static void main(String[] args) {
+        AsistenciaApplication.main(args);
+        System.out.println("Iniciando... application DMP");
+    }
+}
