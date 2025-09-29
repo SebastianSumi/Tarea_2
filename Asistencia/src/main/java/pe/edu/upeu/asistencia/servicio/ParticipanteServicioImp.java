e52bef507ea4bd6b2278bd8cb3628db8e1fea6e1
SebastianSumi <cspam7780@gmail.com>
2025-09-29 12:59:05 -0500

    Mensaje del commit

--- Asistencia/src/main/java/pe/edu/upeu/asistencia/servicio/ParticipanteServicioImp.java
+++ Asistencia/src/main/java/pe/edu/upeu/asistencia/servicio/ParticipanteServicioImp.java
@@ -1,32 +1,35 @@
 package pe.edu.upeu.asistencia.servicio;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pe.edu.upeu.asistencia.modelo.Participante;
-import pe.edu.upeu.asistencia.repositorio.ParticipanteRepository;
+import pe.edu.upeu.asistencia.repositorio.ParticipanteIRepository;
+
 import java.util.List;
 
 @Service
-public class ParticipanteServicioImp extends ParticipanteRepository
-        implements ParticipanteServicioI {
+public class ParticipanteServicioImp implements ParticipanteServicioI {
+    @Autowired
+    ParticipanteIRepository participanteIRepository;
     @Override
     public void save(Participante participante) {
-        super.save(participante);
+        participanteIRepository.save(participante);
     }
     @Override
     public Participante update(Participante participante) {
-        return super.update(participante);
+        return participanteIRepository.save(participante);
     }
     @Override
     public void delete(String dni) {
-        super.delete(dni);
+        participanteIRepository.deleteById(dni);
     }
     @Override
-    public Participante findById(int index) {
-        return participantes.get(index);
+    public Participante findById(String dni) {
+        return participanteIRepository.findById(dni).orElse(null);
     }
 
     @Override
     public List<Participante> findAll(){
-        return super.findAll();
+        return participanteIRepository.findAll();
     }
 }
