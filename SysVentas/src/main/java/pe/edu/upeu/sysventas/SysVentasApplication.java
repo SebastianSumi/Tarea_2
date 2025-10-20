1dff89a50532714efe317db8786b2de9dccdc372
SebastianSumi <cspam7780@gmail.com>
2025-10-20 12:26:17 -0500

    mari5

--- SysVentas/src/main/java/pe/edu/upeu/sysventas/SysVentasApplication.java
+++ SysVentas/src/main/java/pe/edu/upeu/sysventas/SysVentasApplication.java
@@ -31,16 +31,18 @@ public class SysVentasApplication extends Application {
         builder.application().setWebApplicationType(WebApplicationType.NONE);
         ctx=builder.run(getParameters().getRaw().toArray(new
                 String[0]));
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/main_producto.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/login.fxml"));
         loader.setControllerFactory(ctx::getBean);
         parent = loader.load();
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        Screen screen = Screen.getPrimary();
+        /*Screen screen = Screen.getPrimary();
         Rectangle2D bounds = screen.getVisualBounds();
-        stage.setScene(new Scene(parent, bounds.getWidth(), bounds.getHeight()-100));
+        stage.setScene(new Scene(parent, bounds.getWidth(), bounds.getHeight()-100));*/
+        Scene scene = new Scene(parent);
+        stage.setScene(scene);
         stage.setTitle("SysVentas SysCenterLife");
         stage.show();
     }
